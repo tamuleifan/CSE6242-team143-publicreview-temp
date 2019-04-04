f0ea01343dd418065f669a854fcdb43377a42b80
MT Lu <morek999@gmail.com>
2019-04-04 23:18:06 +0800

    update geocodeAddress
    remove listener in iniMap. Make it as a function in Javascript

--- FrontEnd/googlemap.js
+++ FrontEnd/googlemap.js
@@ -1,7 +1,7 @@
 //var map;
 function initMap() {
     // Map options
-    var options = {
+    options = {
       zoom: 15,
       center: {
         lat: 40.7128,
@@ -11,6 +11,18 @@ function initMap() {
     // New map
     map = new google.maps.Map(document.getElementById('map'), options);
 
+    // Limiting map scope (map zoom)
+    var minZoomLevel = 14;
+    map.addListener('zoom_changed', function(){
+      if (map.getZoom() < minZoomLevel) map.setZoom(minZoomLevel);
+    });
+
+    /*/ geocoder
+    var geocoder = new google.maps.Geocoder();
+    document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(geocoder, map);
+    });*/
+
     // Crime heatmap
     heatmap = new google.maps.visualization.HeatmapLayer({
       data: getPoints(),
@@ -19,11 +31,6 @@ function initMap() {
     heatmap.set('radius', 100);
     heatmap.setMap(null);
 
-    // geocoder
-     var geocoder = new google.maps.Geocoder();
-     document.getElementById('submit').addEventListener('click', function() {
-       geocodeAddress(geocoder, map);
-     });
 
      $(document).ready(function(){
        //$("button").click(function(){
@@ -57,85 +64,85 @@ function initMap() {
 
 };
 
-    // customized marker- goldstar
-    /*var goldStar = {
-      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-      fillColor: 'yellow',
-      fillOpacity: 0.8,
-      scale: 0.1,
-      strokeColor: 'gold',
-      strokeWeight: 1
-    };*/
+  // customized marker- goldstar
+  /*var goldStar = {
+    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+    fillColor: 'yellow',
+    fillOpacity: 0.8,
+    scale: 0.1,
+    strokeColor: 'gold',
+    strokeWeight: 1
+  };*/
 
-    /*
-    // Array of markers
-    var markers = [{
-        coords: {
-          lat: 40.7128,
-          lng: -74.0060
-        },
-        iconImage: goldStar,
-        content: "<h3>Liberty Statue</h3>"
+  /*
+  // Array of markers
+  var markers = [{
+      coords: {
+        lat: 40.7128,
+        lng: -74.0060
       },
-      {
-        coords: {
-          lat: 40.758896,
-          lng: -73.985130
-        },
-        iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-        content: "<h3>Times Square</h3>"
+      iconImage: goldStar,
+      content: "<h3>Liberty Statue</h3>"
+    },
+    {
+      coords: {
+        lat: 40.758896,
+        lng: -73.985130
       },
-      {
-        coords: {
-          lat: 40.8075,
-          lng: -73.9626
-        },
-        content: "<h3>Columbia University</h3>"
-      }
-    ];
-    */
+      iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+      content: "<h3>Times Square</h3>"
+    },
+    {
+      coords: {
+        lat: 40.8075,
+        lng: -73.9626
+      },
+      content: "<h3>Columbia University</h3>"
+    }
+  ];
+  */
 
 
 
 
-  /*  for (var i = 0; i < markers.features.length; i++) {
-      addMarker(markers[i]);
-    };
+/*  for (var i = 0; i < markers.features.length; i++) {
+    addMarker(markers[i]);
+  };
 
-    // Add Marker function
-    function addMarker(props) {
-      var marker = new google.maps.Marker({
-        position: props.coords,
-        map: map,
-        //icon:props.iconImage
-      });
-      // Check for customized icon
-      if (props.iconImage) {
-        // Set icon image
-        marker.setIcon(props.iconImage)
-      }
-
-      // Check content
-      if (props.content) {
-        var infoWindow = new google.maps.InfoWindow({
-          content: props.content
-        });
-
-        marker.addListener('click', function() {
-          infoWindow.open(map, marker);
-        });
-      }
+  // Add Marker function
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+      //icon:props.iconImage
+    });
+    // Check for customized icon
+    if (props.iconImage) {
+      // Set icon image
+      marker.setIcon(props.iconImage)
     }
 
-    // Listen for click on map
-    google.maps.event.addListener(map, 'click',
-      function(event) {
-        // Add markers
-        addMarker({
-          coords: event.latLng
-        });
+    // Check content
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content
       });
+
+      marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+      });
+    }
   }
+
+  // Listen for click on map
+  google.maps.event.addListener(map, 'click',
+    function(event) {
+      // Add markers
+      addMarker({
+        coords: event.latLng
+      });
+    });
+}
 */
 
 //ajax asynchronous deprecated
@@ -169,50 +176,50 @@ function toggleHeatmap() {
   heatmap.setMap(heatmap.getMap() ? null : map);
 }
 
-  // translate address to geo location of lat and lng
-  var markers = []
-  function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
+// translate address to geo location of lat and lng
+var markers = []
+function geocodeAddress(address) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            map.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
+                map: map,
+                position: results[0].geometry.location
             });
             markers.push(marker)
             if(markers.length == 2){
-              markers[0].setMap(null);//delete the previous marker
-              markers.shift();//remove the first element in the array
+                markers[0].setMap(null);//delete the previous marker
+                markers.shift();//remove the first element in the array
             }
-          } else {
+        } else {
             alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
-
-      //clear entries and map display
-    function clearEntries(){
-      		$("#address, #filter").val("");
-      		//$("#map, #panel-direction").html("");
-      	}
-
-
-      /*var markers;
-
-      $(document).ready(function(){
-        $("button").click(function(){
-          $.get("toy.csv", function(data, status){
-            if(status == "success"){
-            alert("Data: " + data + "\nStatus: " + status);
-          }
-            Papa.parse(data, {
-              header: true,
-              complete: function(results){
-                markers = results.data;
-              }
-            });
-          });
-        });
+        }
       });
-      */
+    }
+
+//clear entries and map display
+function clearEntries(){
+    $("#address, #filter").val("");
+    //$("#map, #panel-direction").html("");
+}
+
+
+/*var markers;
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $.get("toy.csv", function(data, status){
+      if(status == "success"){
+      alert("Data: " + data + "\nStatus: " + status);
+    }
+      Papa.parse(data, {
+        header: true,
+        complete: function(results){
+          markers = results.data;
+        }
+      });
+    });
+  });
+});
+*/
